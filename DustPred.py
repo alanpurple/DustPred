41cb9331c2f7c436c02fa9e070d2a70d0e7322c1
Alan Anderson <alanpurple@gmail.com>
2019-07-31 00:22:40 +0900

    use LSTM autoencoder

--- DustPred.py
+++ DustPred.py
@@ -20,7 +20,6 @@ def bind_model(model):
 
     def load(path):
         # model=models.load_model(os.path.join(path,'alan_dust_model2.h5'))
-        model=get_simplegru_model()
         model.load_weights(os.path.join(path,'model_alan.tf'))
 
     def infer(path):
@@ -31,50 +30,61 @@ def bind_model(model):
 def inference(path,model):
     test_path = path+'/test_data'
     data=np.load(test_path)
-    region=data[:,0]
+    #region=data[:,0]
+    #month=data[:,2]
     test_dust=[[[elem[2*i],elem[2*i+1]] for i in range(2,7)] for elem in data]
     test_dust=np.asarray(test_dust)
-    pred=model.predict({'dust_input':test_dust,'region_input':region})
-    tiny_pred=pred[0].tolist()
-    micro_pred=pred[1].tolist()
-    result=[[i,[elem,micro_pred[i]]] for i,elem in enumerate(tiny_pred)]
+    pred=model.predict(test_dust).tolist()
+    result=[[i,elem[-1]] for i,elem in enumerate(pred)]
     return result
 
-def region_one_hot(region):
-    return tf.one_hot(region,10)
+#def region_one_hot(region):
+#    return tf.one_hot(region,10)
 
-def month_one_hot(month):
-    return tf.one_hot(month,12)
+#def month_one_hot(month):
+#    return tf.one_hot(month,12)
 
 def get_simplegru_model(num_units=64,num_layers=2,dropout=0.1):
-    inputs=Input((5,2),name='dust_input')
-    region=Input((),name='region_input',dtype=tf.uint8)
-    month=Input((),name='month_input',dtype=tf.uint8)
+    input=Input((5,2),name='dust_input')
+    inputs_expanded=layers.ZeroPadding1D((0,1))(input)
+    #region=Input((),name='region_input',dtype=tf.uint8)
+    #month=Input((),name='month_input',dtype=tf.float32)
     #cells=[layers.GRUCell(num_units) for _ in range(num_layers-1)]
     #cells.append(layers.GRUCell(num_units,dropout=dropout))
     #multi_gru=layers.RNN(cells,name='multi-lstm')
-    multi_gru=layers.LSTM(num_units)
+    multi_gru=layers.LSTM(num_units,activation='relu')
 
-    output=multi_gru(inputs)
+    # batch X num_units
+    output=multi_gru(inputs_expanded)
+
+    #  batch X 6 X num_units 
+    repeated=layers.RepeatVector(6)(output)
+
+    # batch X 6 X num_units
+    lstm_seq=layers.LSTM(num_units,return_sequences=True,activation='relu')(repeated)
 
     #flattened=layers.Flatten()(inputs)
 
     # (batch,num_units)
-    feature=layers.Dense(num_units,activations.relu)(output)
+    #feature=layers.Dense(num_units,activations.relu)(output)
     #region_one=layers.Lambda(region_one_hot,name='region_one_hot')(region)
     #month_one=layers.Lambda(month_one_hot,name='month_one_hot')(month)
     # (batch,num_units+10+12)
     #feature_concat=layers.Concatenate()([feature,month_one])
     #feature_concat=layers.Dropout(0.1)(feature_concat)
-    dense_final=layers.Dense(16,activations.relu)(feature)
+    #dense_final=layers.Dense(16,activations.relu)(feature)
 
-    tiny=layers.Dense(1,name='tiny_dense')(dense_final)
-    micro=layers.Dense(1,name='micro_dense')(dense_final)
+    #tiny=layers.Dense(1,name='tiny_dense')(dense_final)
+    #micro=layers.Dense(1,name='micro_dense')(dense_final)
 
-    model=Model(inputs=[inputs,region,month],outputs=[tiny,micro])
+    #model=Model(inputs=[inputs,region,month],outputs=[tiny,micro])
+
+    final=layers.TimeDistributed(layers.Dense(2))(lstm_seq)
+    model=Model(inputs=input,outputs=final)
+
     return model
 
-EPOCHS=200
+EPOCHS=100
 
 if __name__ == '__main__':
     
@@ -103,10 +113,11 @@ if __name__ == '__main__':
                     loss=losses.MeanSquaredError(),
                     metrics=[metrics.MeanSquaredError()])
 
-        # cbs=[
-        #     callbacks.EarlyStopping(
-        #             monitor='val_loss',
-        #             min_delta=1e-2,patience=2,verbose=1)]
+        cbs=[
+             callbacks.EarlyStopping(
+                     monitor='val_loss',
+                     min_delta=1e-2,patience=4,verbose=1)
+             ]
 
         train_dataset_path = DATASET_PATH + '/train/train_data'
 
@@ -115,8 +126,8 @@ if __name__ == '__main__':
         data=np.load(train_dataset_path)
         labels=np.load(train_label_file)
 
-        region=data[:,0]
-        month=data[:,2]
+        #region=data[:,0]
+        #month=data[:,2]
 
         ### test
         #idx= (month>2)&(month<5)
@@ -129,8 +140,15 @@ if __name__ == '__main__':
         
         train_dust=[[[elem[2*i],elem[2*i+1]] for i in range(2,7)] for elem in data]
         train_dust=np.asarray(train_dust)
-        model.fit({'region_input':region,'month_input':month,'dust_input':train_dust},
-                {'tiny_dense':labels[:,0],'micro_dense':labels[:,1]},
-                batch_size=128,epochs=EPOCHS,validation_split=0.1)
+        #model.fit({'region_input':region,'month_input':month,'dust_input':train_dust},
+        #        {'tiny_dense':labels[:,0],'micro_dense':labels[:,1]},
+        #        batch_size=128,epochs=EPOCHS,validation_split=0.1)
+
+        train_out=[np.array([[elem[2*i],elem[2*i+1]] for i in range(2,7)]) for elem in data]
+        train_out=[np.append(elem,[labels[idx]],0) for idx,elem in enumerate(train_out)]
+        train_out=np.asarray(train_out)
+
+        model.fit(train_dust,train_out,
+                batch_size=64,epochs=EPOCHS,validation_split=0.1,callbacks=cbs)
 
         nsml.save(EPOCHS)
